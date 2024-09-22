64e6e98b6c979b39dda50f219cd6fabf60079905
roycejohn <cuevasroycejohn.com>
2024-09-22 02:31:20 +0200

    start to prisma set up

--- ecommerce-admin/app/(root)/page.tsx
+++ ecommerce-admin/app/(root)/page.tsx
@@ -1,10 +1,24 @@
-import { Button } from "@/components/ui/button";
+"use client";
 
-export default function Home() {
-    return (
-     <div>
-        <Button size="default" variant="outline">Click me</Button>
-     </div>
-    );
-  }
-  
+import { useEffect } from "react";
+
+import { useStoreModal } from "@/hooks/use-store-modal";
+
+const  SetupPage = () => {
+   const onOpen = useStoreModal((state) => state.onOpen);
+   const isOpen = useStoreModal((state) => state.isOpen);
+
+   useEffect(() =>{
+      if(!isOpen){
+         onOpen();
+      }
+   }, [isOpen, onOpen]);
+
+   return (
+      <div className="p-4">
+        Root Page  
+      </div>
+   );
+}
+
+export default SetupPage;
--- ecommerce-admin/app/layout.tsx
+++ ecommerce-admin/app/layout.tsx
@@ -1,17 +1,19 @@
 import type { Metadata } from "next";
-import localFont from "next/font/local";
+import { Inter } from "next/font/google";
+import {
+  ClerkProvider,
+  SignInButton,
+  SignedIn,
+  SignedOut,
+  UserButton
+} from '@clerk/nextjs'
+
+
+import { ModalProvider } from "@/providers/modal-provider";
+
 import "./globals.css";
 
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
-});
+const inter = Inter ({ subsets: ["latin"]})
 
 export const metadata: Metadata = {
   title: "Admin Dashboard",
@@ -24,12 +26,24 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
-      </body>
-    </html>
+    <ClerkProvider>
+      <html lang="en">
+        <body className={inter.className}>
+          <header className="flex justify-between p-5">
+            <h1> Ecomerce Admin </h1>
+            <SignedOut>
+              <SignInButton />
+            </SignedOut>
+            <SignedIn>
+              <UserButton showName />
+            </SignedIn>
+          </header>
+          <main>
+            <ModalProvider/>
+            {children}
+          </main>
+        </body>
+      </html>
+    </ClerkProvider>
   );
 }
