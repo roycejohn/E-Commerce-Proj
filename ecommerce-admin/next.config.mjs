b500e0c333ab9c3ad7d82408954db7daa289b42e
roycejohn <cuevasroycejohn.com>
2024-09-25 01:37:51 +0200

    Billboard setup

--- ecommerce-admin/next.config.mjs
+++ ecommerce-admin/next.config.mjs
@@ -1,4 +1,20 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+// const nextConfig = {};
 
-export default nextConfig;
+// export default nextConfig;
+
+
+const nextConfig = {
+    images: {
+      remotePatterns: [
+        {
+          protocol: 'https',
+          hostname: 'res.cloudinary.com',
+          port: '', // You can specify a port if needed, or leave it empty
+          pathname: '/**', // Path pattern to match any image from Cloudinary
+        },
+      ],
+    },
+  };
+  
+  export default nextConfig;
